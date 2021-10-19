224ab6f1f58ab89702fb7630130afbee2dc62358
Jan Willem <jwemail4-developer@yahoo.com>
2021-10-19 14:58:23 +0200

    primary qualifier

---
+++ src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingServiceImpl.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+@Primary
+@Service
+public class PrimaryGreetingServiceImpl implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World PRIMARY";
+    }
+}
