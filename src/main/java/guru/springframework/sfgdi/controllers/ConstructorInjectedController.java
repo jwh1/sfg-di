86f8232a9b7860479532b4faaad123b869df4e94
Jan Willem <jwemail4-developer@yahoo.com>
2021-10-19 14:14:06 +0200

    spring di

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,15 +1,17 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
-
+    //   @Autowired  //annotation no longer needed with constructor injection
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
-
-        public String getGreeting(){
+    public String getGreeting(){
         return greetingService.sayGreeting();
     }
 }
