64fa1ff618d2e63193f12b619b68d4e2b1362184
Jan Willem <jwemail4-developer@yahoo.com>
2021-10-20 18:02:54 +0200

    life cycle hooks

---
+++ src/main/java/guru/springframework/sfgdi/services/CustomBeanPostProcessor.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof LifeCycleDemoBean) {
+            System.out.println("## - calling beforeInit()");
+            ((LifeCycleDemoBean) bean).beforeInit();
+        }
+        return bean;
+    }
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof LifeCycleDemoBean) {
+            System.out.println("## - calling afterInit()");
+            ((LifeCycleDemoBean) bean).afterInit();
+        }
+        return bean;
+    }
+}
